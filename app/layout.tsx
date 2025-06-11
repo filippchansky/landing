72e770228af979e9c3b389a3155a3c22386888da
filippchansky <feelising@gmail.com>
2025-06-12 01:48:52 +0600

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { LanguageProvider } from '@/lib/language';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={inter.className}>
+        <Analytics/>
         <LanguageProvider>
           {children}
         </LanguageProvider>
